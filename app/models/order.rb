c9df6e0acc4d5e6bd3a462f5e2835388348b9865
OKAY <fokayx@gmail.com>
2016-01-31 17:08:23 +0800

    class close

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,13 @@
 class Order < ActiveRecord::Base
   has_many :order_items
 
+  default_scope {where(deleted_at.nil?)}
+
+  def deleted?
+    deleted_at.present?
+    #訂單是否已刪除,不要真的刪
+  end
+
   state_machine :initial => :pending do
     event :confirm do
       transition :pending => :confirmed
@@ -11,11 +18,11 @@ class Order < ActiveRecord::Base
     end
 
     event :deliver do
-      transistion :ship => :delivered
+      transition :ship => :delivered
     end
 
     event :refund do
-      transistion :confirm => :refunded
+      transition :confirm => :refunded
     end
 
     event :complian_shipping do
